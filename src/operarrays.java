fea00f8391b9bf4cba64966ccb4e97352ef894d9
antonio0354 <antoniorodri0354@gmail.com>
2019-03-11 11:01:19 +0100

    rama2

--- src/operarrays.java
+++ src/operarrays.java
@@ -4,7 +4,8 @@ public class operarrays {
 	public static int array[] = new int[10];
 	public static void main(String[] args) {
 		rellenarray();
-
+		imprimearray();
+		sumarray();
 	}
 	
 	public static void rellenarray() {
@@ -13,6 +14,22 @@ public class operarrays {
 			
 		}
 		
+	}
+	
+	public static void imprimearray() {
+		for(int i=0;i<array.length;i++) {
+			System.out.print(array[i]+" ");
+			
+		}
+		
+	}
+	//hola3
+	public static void sumarray() {
+		int suma=0;
+		for(int i=0;i<array.length;i++) {
+			suma=suma+array[i];
+		}
+		System.out.println("La suma es: "+suma);
 	}
 
 }
